04a60879c5c312a8cc2bb1bbd24c3491a5746ec8
CMEONE <contact@kabirwearsato.ga>
2024-01-16 17:22:11 -0800

    Add info to reference

--- compile.js
+++ compile.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let header = [];
 header.push("# Reference");
-header.push("");
+header.push("These are the properties that currently exist in the TPS repository, but they are not a required list of data that needs to be submitted. If you collect data that does not fit within one of the existing properties, please add a property file under [/properties](/properties) by submitting a pull request so that the data can be standardized and interoperable!");
 header.push("## Table of Contents");
 header.push("- [Interfaces](#interfaces)");
 header.push("- [Properties](#properties)");
